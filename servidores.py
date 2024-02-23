c9631b18fca0d2cb01d6fe0a97b8a51489c6bec1
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2024-02-23 17:00:40 +0000

    👔 roda um ServidorTCP()

--- servidores.py
+++ servidores.py
@@ -1,3 +1,5 @@
+import utils
+
 class ServidorTCP():
     def __init__(self, host, porta, familia):
         self.host = host
@@ -7,5 +9,10 @@ class ServidorTCP():
     def __str__(self):
         return "[servidor TCP ({}) configurado para a máquina {} e porta {}]".format(self.familia, self.host, self.porta)
 
+    def start(self):
+        server_socket = utils.instanciarSocket(utils.getFamilia(self.familia.upper()), utils.getTipoDeSocket("UDP"))
+        utils.configurarSocket(server_socket, self.host, self.porta)
+
 s = ServidorTCP('127.0.0.1', 1235, "IPV4")
 print(s)
+s.start()
