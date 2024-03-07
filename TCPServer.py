57b23014c3771aa0eeb42c8765aeb996a8634461
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2024-03-07 18:49:28 +0000

    🎨 descreve o método bind()

--- TCPServer.py
+++ TCPServer.py
@@ -2,6 +2,10 @@ import utils
 import socket
 
 class ServidorTCP():
+    def configurarEnderecoDoServidor(self, maquina, porta):
+        endereco = (maquina, porta)
+        self.socket_servidor.bind(endereco)
+    
     def instanciarSocket(self, familia):
         familia_de_sockets = utils.getFamilia(familia)
         instancia = socket.socket(familia_de_sockets, socket.SOCK_STREAM)
@@ -11,9 +15,11 @@ class ServidorTCP():
         self.familia = familia
         self.maquina = maquina
         self.porta = porta
-        socket_servidor = self.instanciarSocket(self.familia)
-        socket_servidor.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        endereco_do_servidor = (maquina, porta)
+        self.socket_servidor = self.instanciarSocket(self.familia)
+        self.socket_servidor.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.configurarEnderecoDoServidor(self.maquina, self.porta)
+        
+
         
 s = ServidorTCP("IPV4", 'localhost', 8082)
 print(s)
