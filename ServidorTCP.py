684944d1a5499f34e03c8d0f5caeb9ecf67b02e7
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2024-03-08 17:11:25 +0000

    🎨 deixa ServidorTCP() mais flexível para o desenvolvedor

--- ServidorTCP.py
+++ ServidorTCP.py
@@ -2,16 +2,52 @@ import utils
 import socket
 
 class ServidorTCP():
+    def encerrarConexao(self):
+        if self.socket_cliente:
+            self.socket_cliente.close()
+            print("[🔌 Conexão encerrada com %s]" % str(self.endereco_cliente))
+            self.socket_cliente, self.endereco_cliente = None, None
+        else:
+            print("[❌ Nenhuma conexão ativa para encerrar]")
+
+    def receberDados(self, tamanho_maximo):
+        if self.socket_cliente:
+            try:
+                dados_codificados = self.socket_cliente.recv(tamanho_maximo)
+                if dados_codificados:
+                    dados_decodificados = dados_codificados.decode("utf-8")
+                    print("[📦 dados recebidos de %s]:" % str(self.endereco_cliente))
+                    print("    [%s]" % dados_decodificados)
+                return dados_decodificados
+            except socket.error as e:
+                print("[❌ Erro ao receber dados: %s]" % str(e))
+            else:
+                print("[❌ Nenhuma conexão ativa para receber dados]")
+            return None
+
+    def enviarDados(self, mensagem):
+        if self.socket_cliente:
+            try:
+                mensagem_codificada = mensagem.encode("utf-8")
+                self.socket_cliente.send(mensagem_codificada)
+            except socket.error as e:
+                print("[❌ Erro ao enviar dados: %s]" % str(e))
+        else:
+            print("[❌ Nenhuma conexão ativa para enviar dados]")
+
+    def aceitarConexao(self):
+        print("[🎧 Servidor TCP em modo de escuta no endereço %s]" % str(self.maquina + ":" + str(self.porta)))
+        self.socket_cliente, self.endereco_cliente = self.socket_servidor.accept()
+        print("[✅ Conexão aceita de %s]" % str(self.endereco_cliente))
+
     def loop(self, tamanho_maximo):
         while True:
-            print("[🎧 servidor TCP em modo de escuta no endereço %s]" % str(self.maquina + ":" + str(self.porta)))
-            socket_cliente, endereco_cliente = self.socket_servidor.accept()
-            dados = socket_cliente.recv(tamanho_maximo)
+            dados = self.socket_cliente.recv(tamanho_maximo)
             if dados:
-                print("[📦 dados recebidos de %s]:" % str(endereco_cliente))
+                print("[📦 dados recebidos de %s]:" % str(self.endereco_cliente))
                 print("    [%s]" % dados.decode('utf-8'))
-                socket_cliente.send("Olá, cliente =)".encode('utf-8'))
-                socket_cliente.close()
+                self.socket_cliente.send("Olá, cliente =)".encode('utf-8'))
+                self.socket_cliente.close()
                 # TODO mais coisas
 
 
@@ -31,11 +67,13 @@ class ServidorTCP():
         self.familia = familia
         self.maquina = maquina
         self.porta = porta
+        self.socket_cliente, self.endereco_cliente = None, None
         self.socket_servidor = self.instanciarSocket(self.familia)
         self.socket_servidor.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.configurarSocketParaEscutarNoEndereco(self.maquina, self.porta)
         self.habilitarModoDeEscuta(5)
         
 s = ServidorTCP("IPV4", 'localhost', 8082)
-s.loop(2048)
-print(s)
+s.aceitarConexao()
+s.enviarDados("Olá, cliente!")
+s.receberDados(2048)
