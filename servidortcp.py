1aa89fb93f0df7117a407dc177f06b225cbe44d6
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2024-03-27 00:41:27 +0000

    aimds não subiu tudo

--- servidortcp.py
+++ servidortcp.py
@@ -13,11 +13,19 @@ PORTA = sys.argv[3]
 BUFFER = sys.argv[4]
 FILA = sys.argv[5]
 
+def receberBase64PorPartes(self):
+    base64_data = b''  # Inicializa uma string de bytes vazia para armazenar os dados recebidos
+    while True:
+        fragment = self.receberBase64()  # Recebe um fragmento de dados
+        if not fragment:  # Verifica se não há mais dados para receber
+            break
+        base64_data += fragment  # Concatena o fragmento recebido à string de bytes
+    return base64_data
+
 servidor = ServidorTCP(FAMILIA_DE_SOCKETS, MAQUINA, PORTA, BUFFER, FILA)
 servidor.aceitarConexao()
 
-base64_recebido = servidor.receberBase64() # TIPO BYTES
-
+base64_recebido = receberBase64PorPartes(servidor)
 string_do_base64 = base64.b64decode(base64_recebido) # ERROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO 
 resultado = open('saida.png', 'wb') # cria uma imagem e permite escrever nela
 resultado.write(string_do_base64)
